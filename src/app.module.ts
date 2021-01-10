84228a0b970483cd15c1e35f91d47560a171f8ff
Igor Polyakov <bitshift.it@icloud.com>
2021-01-10 16:38:38 +0300

    add healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import appConfiguration from './app.configuration';
 import botConfiguration from './bot/bot.configuration';
 import { BotModule } from './bot/bot.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     BotModule,
+    HealthModule,
     ConfigModule.forRoot({
       load: [botConfiguration, appConfiguration],
     }),
