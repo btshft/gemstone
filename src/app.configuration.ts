d83d5ab2877fb722409e96d5a977551776b085a5
Igor Polyakov <bitshift.it@icloud.com>
2021-01-10 16:47:47 +0300

    bind to any interface

--- src/app.configuration.ts
+++ src/app.configuration.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT) || 3000,
+  port: parseInt(process.env.APP_PORT) || 8080,
 }));
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     app.use(bot.webhookCallback(path));
   }
 
-  app.listen(port);
+  app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
